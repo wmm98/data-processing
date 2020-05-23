ed152c974acd4aff4c354975a239ee9b26b61248
lenovo <792545884@qq.com>
2020-05-23 18:40:46 +0800

    分组柱状图（多维，纵向）

---
+++ 数据可视化/分组柱状图（二维）.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+students = pd.read_excel("D:\课\数据分析表\Students_data.xlsx")
+# 排序数据
+students.sort_values(by=[2016, 2017], inplace=True, ascending=[False, False])
+# 此plot为DataFrame的plot
+students.plot.bar(x='Field', y=[2016, 2017], color=['orange', 'red', 'blue'])
+plt.title('international Student by Field', fontsize=16, fontweight='bold')
+plt.xlabel('Field', fontweight='bold')
+plt.ylabel('Number', fontweight='bold')
+# 调整整个图X坐标的位置
+"""
+rotation旋转45度
+ ha='right'以右为中心点
+"""
+ax = plt.gca()
+ax.set_xticklabels(students['Field'], rotation=45, ha='right')
+# 调整图的整个位置，去除多余空白
+f = plt.gcf()
+# 调左边和底部的位置
+f.subplots_adjust(left=0.2, bottom=0.42)
+plt.tight_layout()
+plt.show()
+# print(students)
